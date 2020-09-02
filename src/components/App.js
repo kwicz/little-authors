fb4da12e1f72afcfdea82db0f9b2f6188f41a0b4
Katy Solovewicz <katy@solovewicz.com>
2020-09-02 15:43:03 -0700

    add booklist, edit book, and create book components

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,35 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../App.css'
+import CreateBook from "./CreateBook.component";
+import EditBook from "./EditBook.component";
+import BookList from "./BookList.component";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
-          <h2>Little Authors</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="https://k.solovewi.cz/" target="_blank" rel="noopener noreferrer">
+              <img src="" width="30" height="30" alt="" />
+            </a>
+            <Link to="/" className="navbar-brand">Little Authors</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Home</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Book</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/" exact component={BookList} />
+          <Route path="/edit/:id" component={EditBook} />
+          <Route path="/create" component={CreateBook} />
         </div>
       </Router>
     );
---
+++ src/components/CreateBook.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateBook extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Create Book Component!!</p>
+            </div>
+        )
+    }
+}
